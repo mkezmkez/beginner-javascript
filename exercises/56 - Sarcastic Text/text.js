7299ced5668e51b3b8e26835dae3b1fa6c43f996
Luis Puente <puente.luism@gmail.com>
2020-06-04 13:47:58 -0500

    initial commit

---
+++ exercises/56 - Sarcastic Text/text.js
@@ -0,0 +1,7 @@
+const textArea = document.querySelector('[name="text"');
+const result = document.querySelector('.result');
+const filterInputs = document.querySelectorAll('[name="filter]')
+
+console.log(textarea,result,filterInputs)
+result;
+ 
